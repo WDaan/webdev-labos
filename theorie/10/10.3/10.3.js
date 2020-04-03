6caf124797901b340282a0abf914db11a64eceb9
Daan Wijns <dw.daanwijns@gmail.com>
2020-04-03 12:51:39 +0200

    theo 10

--- theorie/10/10.3/10.3.js
+++ theorie/10/10.3/10.3.js
@@ -1,9 +1,18 @@
 function add() {
-  $("#title").addClass("background");
+  $("#title-wrapper").addClass("background");
+  $("table").addClass("table");
+  $("ul").addClass("list-group");
+  $("li").addClass("list-group-item");
 }
 function reset() {
-  $("#title").removeClass("background").removeClass("font");
+  $("#title").removeClass("font");
+  $("#title-wrapper").removeClass("background");
+  $("table").removeClass("table");
+  $("ul").removeClass("list-group");
+  $("li").removeClass("list-group-item");
 }
 function toggle() {
   $("#title").toggleClass("font");
 }
+
+// 1 div, 1 link && 1 tabel aanpassen!!!
